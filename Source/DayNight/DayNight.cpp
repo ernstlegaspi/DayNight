9dc12fef8b231d5d5c193967788bb9435b6fc318
Akie <legaspiernstdanielle@gmail.com>
2023-01-15 21:13:39 +0800

    Add project files.

---
+++ Source/DayNight/DayNight.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DayNight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DayNight, "DayNight" );
---
+++ Source/DayNight/DayNightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DayNightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAYNIGHT_API ADayNightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
