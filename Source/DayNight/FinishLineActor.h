850a7b49fea502fc8ea58d45d400acc876d6af6f
Akie <legaspiernstdanielle@gmail.com>
2023-01-15 23:36:55 +0800

    Added platform manager and wooden lamp
    Added platform manager and wooden lamp

--- Source/DayNight/FinishLineActor.h
+++ Source/DayNight/FinishLineActor.h
@@ -17,10 +17,13 @@ public:
 	UPROPERTY(EditAnywhere)
 	class UStaticMeshComponent* TriggerMesh;
 
+	UPROPERTY(EditAnywhere)
+	float AppleCountInStage;
+
 	UFUNCTION()
 	void OnFinishBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	short int AppleCount, AppleCountInStage;
+	short int AppleCount;
 
 protected:
 	virtual void BeginPlay() override;
