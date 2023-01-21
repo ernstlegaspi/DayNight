863c5ed95375376765ebac90c91ddd429813db6a
Akie <legaspiernstdanielle@gmail.com>
2023-01-21 20:27:43 +0800

    Added Gameplaycameras and niagara in module
    Added Gameplaycameras and niagara in module

--- Source/DayNight/DayNight.Build.cs
+++ Source/DayNight/DayNight.Build.cs
@@ -4,7 +4,7 @@ public class DayNight : ModuleRules {
 	public DayNight(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "GameplayCameras", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 	}
